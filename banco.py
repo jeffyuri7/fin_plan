066bef355a6b70b691b75d6a8bef65e6c796cc1b
Jefferson Yuri <jeffyuri@hotmail.com>
2021-11-02 23:47:36 -0300

    Criação da classe Banco e dos métodos relacionados a tabela despesas

---
+++ banco.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import sqlite3
+
+class Banco:
+    def __init_(self, arquivo):
+        self.arquivo = arquivo
--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-builder = Gtk.Builder()
-builder.add_from_file('ui.glade')
-
 class Manipulador:
     def on_main_window_destroy(self, window):
         Gtk.main_quit()
 
+builder = Gtk.Builder()
+builder.add_from_file('ui.glade')
 builder.connect_signals(Manipulador())
 window = builder.get_object('main_window')
 window.show_all()
