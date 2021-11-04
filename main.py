762a0d0ac41a5f0f6d03a67f9d31631f6876c62a
Jefferson Yuri <jeffyuri@hotmail.com>
2021-11-04 18:37:31 -0300

    Criação dos métodos __init__ e on_btn_inserir_orcamento_clicked da classe main.py

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class Manipulador:
+    def __init__(self):
+        self.armazenamento: Gtk.ListStore = builder.get_object('liststore1')
+        self.Stack: Gtk.Stack = builder.get_object('stack')
+
     def on_main_window_destroy(self, window):
         Gtk.main_quit()
+
+
 
 builder = Gtk.Builder()
 builder.add_from_file('ui.glade')
