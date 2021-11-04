9bd3b1b5d2204114f6b8953cbae9a269122d9a01
Jefferson Yuri <jeffyuri@hotmail.com>
2021-11-04 19:19:01 -0300

    Método atualizar_orcamento implementado e testado - Primeira parte

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from banco import Banco
+from resumo import Resumo
 import gi
 
 gi.require_version('Gtk', '3.0')
@@ -9,10 +11,18 @@ class Manipulador:
     def __init__(self):
         self.armazenamento: Gtk.ListStore = builder.get_object('liststore1')
         self.Stack: Gtk.Stack = builder.get_object('stack')
+        self.banco = Banco('bancodedados.db')
 
     def on_main_window_destroy(self, window):
         Gtk.main_quit()
 
+    def on_btn_inserir_orcamento_clicked(self, button):
+        orcamento = builder.get_object('ent_valor_disp').get_text()
+        self.atualizar_orcamento(orcamento)
+
+    def atualizar_orcamento(self, orcamento):
+        resumo = Resumo(self.banco)
+        print(resumo.saldo_dia)
 
 
 builder = Gtk.Builder()
