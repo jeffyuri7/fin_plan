f29c62086e5648e9b0c234fa307da472a29b3775
Jefferson Yuri <jeffyuri@hotmail.com>
2021-11-05 23:19:53 -0300

    Criação da view_adicionar e implementação dos comandos para ir e retornar dela

--- main.py
+++ main.py
@@ -13,28 +13,36 @@ class Manipulador:
         self.armazenamento: Gtk.ListStore = builder.get_object('liststore1')
         self.Stack: Gtk.Stack = builder.get_object('stack')
         self.banco = Banco('bancodedados.db')
-        self.atualizar_orcamento()
-        self.atualizar_lista_de_despesas()
+        # Atualiza os dados de resumo e a lista de despesas no momento da abertura do software
+        self.atualizar_tela()
 
     def on_main_window_destroy(self, window):
+        self.banco.fechar()
         Gtk.main_quit()
 
-    def on_btn_inserir_orcamento_clicked(self, button):
-        orcamento = builder.get_object('ent_valor_disp').get_text()
-        self.atualizar_orcamento(orcamento)
-
-    def atualizar_orcamento(self):
+    def atualizar_tela(self):
         resumo = Resumo(self.banco)
         builder.get_object('lbl_valor_gasto_realizado').set_text(str(f'R$ {resumo.gasto:.2f}').replace('.',','))
         builder.get_object('lbl_saldo_dia').set_text(str(f'R$ {resumo.saldo_dia:.2f}').replace('.',','))
         builder.get_object('lbl_media_por_dia').set_text(str(f'R$ {resumo.media_dia:.2f}').replace('.',','))
         builder.get_object('lbl_valor_saldo_disp').set_text(str(f'R$ {resumo.saldo_disponivel:.2f}').replace('.',','))
-
-    def atualizar_lista_de_despesas(self):
         lista_despesas = Despesa(self.banco).lista_de_dados
         self.armazenamento.clear()
         for despesa in lista_despesas:
             self.armazenamento.append((despesa[1], despesa[2], str(f'R$ {despesa[3]:.2f}').replace('.',',')))
+
+    def on_btn_inserir_orcamento_clicked(self, button):
+        orcamento = builder.get_object('ent_valor_disp').get_text()
+        #self.atualizar_orcamento(orcamento)
+
+    def on_btn_adicionar_clicked(self, button):
+        self.Stack.set_visible_child_name('view_adicionar')
+
+    def on_btn_cancelar_clicked(self, button):
+        self.Stack.set_visible_child_name('view_principal')
+
+    #def on_btn_adicionar_confirmar_clicked(self, button):
+        #data = builder.get_object('ent_data').get_text()
 
 builder = Gtk.Builder()
 builder.add_from_file('ui.glade')
